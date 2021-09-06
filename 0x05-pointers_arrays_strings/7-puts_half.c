a5f06f9c97b12ef98636806a5d53e1cd36749f97
Ogega254 <ogegagideon9@gmail.com>
2021-09-06 05:49:39 -0700

    printing half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: string.
+ *
+ */
+void puts_half(char *str)
+{
+	int half, length = _strlen(str);
+	int i;
+
+	if (length % 2 == 0)
+		half = length / 2;
+	else
+		half = (length - 1) / 2;
+
+	i = length - half;
+	while (i < length)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
