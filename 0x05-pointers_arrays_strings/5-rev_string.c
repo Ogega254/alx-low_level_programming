05773aa7431aa2704617522a5b4c1028559618f2
Ogega254 <ogegagideon9@gmail.com>
2021-09-06 05:40:44 -0700

    reversing string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverses a string.
+ * @s: string.
+ *
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char aux;
+
+	i = _strlen(s);
+	i--;
+	for (j = 0; j <= i; j++, i--)
+	{
+		aux = s[j];
+		s[j] = s[i];
+		s[i] = aux;
+	}
+}
