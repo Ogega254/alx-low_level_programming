c1db292f7d37681792cb30667ef90ae7dac20115
Ogega254 <ogegagideon9@gmail.com>
2021-09-15 10:44:17 -0700

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,49 +1,21 @@
 #include "main.h"
-#include "2-strlen_recursion.c"
 
 /**
- * compare - auxiliar function (recursive).
- * @s1: string 1.
- * @s2: string 2.
- *
- * Return: 1 or 0.
+ * wildcmp - compare two strings and returns 1 if identical
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical otherwise 0
  */
-int compare(char *s1, char *s2)
-{
-	if (*s1 == 0 && *s2 == 0)
-		return (1);
-	if (*s1 == 0 && *s2 == '*')
-		return (compare(s1, s2 + 1));
-	if (*s1 == 0 || *s2 == 0)
-		return (0);
-	if (*s1 == *s2)
-		return (compare(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return (compare(s1, (s2 + 1)));
-		if (compare(s1, s2 + 1) == 1)
-			return (1);
-		return (compare(s1 + 1, s2));
-	}
-	return (0);
-}
 
-
-/**
- * wildcmp - compares two strings.
- * returns 1 if the strings can be considered identical, otherwise return 0.
- * @s1: string 1.
- * @s2: string 2.
- *
- * Return: 1 or 0.
- */
 int wildcmp(char *s1, char *s2)
 {
-	int s1_length = _strlen_recursion(s1);
-	int s2_length = _strlen_recursion(s2);
+int j = 0;
 
-	if (s1_length != s2_length)
-		return (0);
-	return (compare(s1, s2));
+if (*(s1 + j) == '\0' && *(s2 + j) == '\0')
+return (1);
+if (*(s1 + j) == *(s2 + j))
+return (1 && wildcmp(s1 + 1, s2 + 1));
+if ((s2 + j) == '')
+return (wildcmp(s1, s2 + 1) || (*(s1 + j) != '\0' && wildcmp(s1 + 1, s2)));
+return (0);
 }
