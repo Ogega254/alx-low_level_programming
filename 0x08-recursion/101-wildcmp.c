d9ba92f61fc2f192f281dd17892269c6b26e6c78
Ogega254 <ogegagideon9@gmail.com>
2021-09-15 10:55:00 -0700

    wild cmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,58 +1,49 @@
 #include "main.h"
+#include "2-strlen_recursion.c"
 
 /**
- * wildcmp - compare two strings with "wildcard expansion" capabilities
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if strings can be considered identical, else 0
+ * compare - auxiliar function (recursive).
+ * @s1: string 1.
+ * @s2: string 2.
+ *
+ * Return: 1 or 0.
  */
-
-int wildcmp(char *s1, char *s2)
+int compare(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == 0 && *s2 == 0)
 		return (1);
-	else if (*s1 == '\0' || *s2 == '\0')
-	{
-		if (s1 == '\0' && *s2 == '')
-			return wildcmp(s1, ++s2);
-		else if (s1 == '' && *s2 == '\0')
-			return wildcmp(++s1, s2);
+	if (*s1 == 0 && *s2 == '*')
+		return (compare(s1, s2 + 1));
+	if (*s1 == 0 || *s2 == 0)
 		return (0);
-	}
-
 	if (*s1 == *s2)
+		return (compare(s1 + 1, s2 + 1));
+	if (*s2 == '*')
 	{
-		return wildcmp(++s1, ++s2);
+		if (*(s2 + 1) == '*')
+			return (compare(s1, (s2 + 1)));
+		if (compare(s1, s2 + 1) == 1)
+			return (1);
+		return (compare(s1 + 1, s2));
 	}
-	else if (s1 == '')
-	{
-		if ((s1 + 1) == '')
-			return wildcmp(++s1, s2);
-		else
-		{
-			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
-		}
-	}
-	else if (s2 == '')
-	{
-		if ((s2 + 1) == '')
-			return wildcmp(s1, ++s2);
-		else
-		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
-		}
-	}
-
 	return (0);
-
 }
 
-int findsrc(char *s, char c, int i, int p)
-{
-	if (*(s + i) == '\0')
-		return (p + 1);
-	else if ((s + i) == c || *(s + i) == '')
-		p = i;
 
-	return (findsrc(s, c, i + 1, p));
+/**
+ * wildcmp - compares two strings.
+ * returns 1 if the strings can be considered identical, otherwise return 0.
+ * @s1: string 1.
+ * @s2: string 2.
+ *
+ * Return: 1 or 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int s1_length = _strlen_recursion(s1);
+	int s2_length = _strlen_recursion(s2);
+
+	if (s1_length != s2_length)
+		return (0);
+	return (compare(s1, s2));
 }
