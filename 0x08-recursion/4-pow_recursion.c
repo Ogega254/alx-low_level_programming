3534a9dbe9fa4b5389f97a4166f1d88c802230be
Ogega254 <ogegagideon9@gmail.com>
2021-09-15 10:17:43 -0700

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * _pow_recursion - Returns the value of x raised to the power of y
@@ -17,3 +17,4 @@ int _pow_recursion(int x, int y)
 
 	return (x * _pow_recursion(x, y - 1));
 }
+
