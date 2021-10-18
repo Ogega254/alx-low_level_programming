a540b54ed157377c03202eb5d6ac0a17a178831e
Ogega254 <ogegagideon9@gmail.com>
2021-10-18 03:20:46 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,12 @@
-0x15. C - File I/O
-Learning Objectives
-Look for the right source of information online
-How to create, open, close, read and write files
-What are file descriptors
-What are the 3 standard file descriptors, what are their purpose and what are their POSIX names
-How to use the I/O system calls open, close, read and write
-What are and how to use the flags O_RDONLY, O_WRONLY, O_RDWR
-What are file permissions, and how to set them when creating a file with the open system call
-What is a system call
-What is the difference between a function and a system call
+#ifndef FILE_IO
+#define FILE_IO
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
